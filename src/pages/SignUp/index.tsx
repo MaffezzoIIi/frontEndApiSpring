4e2a2f1aeb07c0ea328920095d5282f10ce32c66
Thomas Maffe <thomasmaffezzolli8@gmail.com>
2021-08-06 10:28:49 -0300

    Realizar cadastro no sistema

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,59 +3,91 @@ import { FiArrowLeft, FiUser, FiLock } from "react-icons/fi";
 import { Form } from "@unform/web";
 import { FormHandles } from "@unform/core";
 import * as Yup from "yup";
+import { Link, useHistory } from "react-router-dom";
+
+import api from "../../service/api";
+
+import { useToast } from "../../hooks/toast";
+
 import getValidationErrors from "../../utils/getValidationErrors";
 
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
-import { Container, Content , Background } from "./style";
+import { Container, Content, Background } from "./style";
 
-const SigUp: React.FC = () => {
-    const formRef = useRef<FormHandles>(null);
+interface SignUpFormData {
+  email: string;
+  senha: string;
+}
 
-    const handleSubmit = useCallback(async (data: object) => {
-        try {
-          formRef.current?.setErrors({});
-    
-          const schema = Yup.object().shape({
-            email: Yup.string()
-              .required("E-mail obrigatório")
-              .email("Informe um e-mail válido"),
-            senha: Yup.string().min(6, "No mínimo 6 digitos"),
-          });
-    
-          await schema.validate(data, {
-            abortEarly: false,
-          });
-        } catch (err) {
-          const errors = getValidationErrors(err);
-          formRef.current?.setErrors(errors);
-        }
-      }, []);
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
 
-      return (
-          <Container>
-              <Background />
-              <Content>
-                  <Form ref={formRef} onSubmit={handleSubmit}>
-                      <h1>Faça seu cadastro</h1>
+  const handleSubmit = useCallback(async (data: SignUpFormData) => {
+    try {
+      formRef.current?.setErrors({});
 
-                     <Input icon={FiUser} name="email" placeholder="E-mail" />
-                     <Input 
-                        icon={FiLock}
-                        name="senha"
-                        type="password"
-                        placeholder="Senha"
-                      />
-                      <Button type="submit">Cadastrar</Button>
-                  </Form>
-                  <a href="#">
-                      <FiArrowLeft />
-                      Voltar para o login
-                  </a>
-                  </Content>
-          </Container>
-      );
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Informe um e-mail válido"),
+        senha: Yup.string().min(6, "No mínimo 6 digitos"),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      history.push('/');
+
+      await api.post('/pessoas/usuario', data);
+
+      addToast({
+        type: 'success',
+        title: 'Cadastro realizado ',
+        description: 'Usuario cadastrado com sucesso! Você já pode relalizar o login'
+      })
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+        return;
+      }
+
+      addToast({
+        type: 'error',
+        title: 'Erro no cadastro',
+        description: 'Ocorreu um erro ao fazer o cadastro, tente novamento!'
+      })
+    }
+  }, [history]);
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input icon={FiUser} name="email" placeholder="E-mail" />
+          <Input
+            icon={FiLock}
+            name="senha"
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para o login
+        </Link>
+      </Content>
+    </Container>
+  );
 };
 
-export default SigUp;
+export default SignUp;
